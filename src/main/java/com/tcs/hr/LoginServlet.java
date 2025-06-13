472db6475ac608a1f8f7efc41d7babc8e2757e4c
DovetailSolution <dovetailsolutions9@gmail.com>
2025-06-13 13:16:21 +0530

    second

--- src/main/java/com/tcs/hr/LoginServlet.java
+++ src/main/java/com/tcs/hr/LoginServlet.java
@@ -42,7 +42,7 @@ private static Key generateKey() {
 }
 
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String registeredEmpid = request.getParameter("empid");
+    String registeredEmpId = request.getParameter("empid");
     //String empId= request.getParameter("empid");
     String password = request.getParameter("password");
     String latitude = request.getParameter("latitude");
@@ -77,7 +77,7 @@ protected void doPost(HttpServletRequest request, HttpServletResponse response) 
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/attendance_db", "root", "manager");
         
         PreparedStatement ps = con.prepareStatement("SELECT id, empId, username, role FROM users WHERE empId=? AND password=?");
-        ps.setString(1, registeredEmpid);
+        ps.setString(1, registeredEmpId);
         ps.setString(2, password);
         ResultSet rs = ps.executeQuery();
 
